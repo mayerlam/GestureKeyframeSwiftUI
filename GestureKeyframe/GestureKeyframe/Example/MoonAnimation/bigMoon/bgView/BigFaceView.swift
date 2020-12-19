eeb8571ba6312ef4e9931243e7264aad5df474f3
Mayer Lam <mayerdev01@gmail.com>
2020-12-19 23:15:46 +0800

    Example 第一輪調試

--- GestureKeyframe/GestureKeyframe/Example/MoonAnimation/bigMoon/bgView/BigFaceView.swift
+++ GestureKeyframe/GestureKeyframe/Example/MoonAnimation/bigMoon/bgView/BigFaceView.swift
@@ -9,113 +9,90 @@ import SwiftUI
 
 struct BigFaceView: View {
 
-    @State var pos: CGSize
-    @State var position: CGSize
-    
-    private let Eye: some View = {
+    @Binding var gesture: CGSize
+
+    private let eye: some View = {
         Circle()
             .foregroundColor(Color.black)
+    } ()
+    
+    private let cheek: some View = {
+        Circle()
+            .foregroundColor(Color(UIColor.init(hex: 0x0A97FB)))
     } ()
     
     var body: some View {
         GeometryReader { geo in
             let scale = min(geo.size.width, geo.size.height) / 270
-            let leftScale = (position).width < 0 ? 1 + scaleSpeed(position.width, scale) : 1
-            let rightScale = position.width > 0 ? 1 - scaleSpeed(position.width, scale) : 1
-            
-            VStack {
-                Spacer()
-                HStack {
-                    
-                    // Left eye.
-                    Eye
-                        .frame(width: size.0 * scale, height: size.0 * scale)
-                        .scaleEffect(leftScale)
+            Keyframe(
+                CGPoint(x: gesture.width, y: gesture.height),
+                timeLine: keyPoint,
+                curveType: .line) { out in
+
+                let leftEye = BFKFParameters.leftEye
+                let rightEye = BFKFParameters.rightEye
+                let mouth = BFKFParameters.mouth
+                let tongue = BFKFParameters.tongus
+
+                VStack {
+                    Spacer()
+                    HStack {
+                        // Left eye.
+                        eye
+                            .frame(width: size.0 * scale, height: size.0 * scale)
+                            .scaleEffect(out(leftEye.scale!, .x))
+                        Spacer()
+                        // Right eye.
+                        eye
+                            .frame(width: size.0 * scale, height: size.0 * scale)
+                            .scaleEffect(out(rightEye.scale!, .x))
+                    }
+                    .frame(width: size.1 * scale, height: size.0 * scale)
+                    .offset(x: out(rightEye.xOffset!, .x), y: out(rightEye.yOffset!, .y))
+
+                    //  Mouth stack
+                    ZStack(alignment: .bottom) {
+                        Image("mouth").resizable()
+                            .frame(
+                                width: out(mouth.width!, .x),
+                                height: 19 * scale)
+                        Image("tongue").resizable()
+                            .frame(
+                                width: 16 * scale,
+                                height: out(tongue.height!, .y)
+                            )
+                            .scaleEffect(out(tongue.scale!, .y))
+                    }
+                    .offset(x: out(mouth.xOffset!, .x), y: out(mouth.yOffset!, .y))
 
                     Spacer()
-                    // Right eye.
-                    Eye
-                        .frame(width: size.0 * scale, height: size.0 * scale)
-                        .scaleEffect(rightScale)
                 }
-                .frame(width: size.1 * scale, height: size.0 * scale)
-                .offset(x: xOffsetSpeed(position.width, 30.0), y: yOffsetSpeed(position.height, 30.0))
-
-                // Mouth
-                ZStack(alignment: .bottom) {
-                    Image("mouth").resizable()
-                        .frame(
-                            width: mouth.0 * scale - (position.width > 0 ? (15 / 105 * position.width) : 0),
-                            height: mouth.1 * scale)
-                    Image("tongue").resizable()
-                        .frame(width: tongue.0 * scale, height: tongue.1 * scale + (position.height < 0 ? (4.0 / 93.0) * position.height : 0))
-                        .scaleEffect(1.0 - (position.width > 0 ? (0.4 / 105 * position.width) : 0))
-                        
-                }
-                .offset(x: xOffsetSpeed(position.width, 34.0), y: yOffsetSpeed(position.height, 35.0))
-                    
-                Spacer()
+                .frame(width: geo.size.width, height: geo.size.height)
             }
-            .frame(width: geo.size.width, height: geo.size.height)
-            .gesture(
-                DragGesture()
-                    .onChanged({ (value) in
-                        if abs(xOffsetSpeed(value.translation.width + self.pos.width, 30.0)) <= 30 * scale {
-                            self.position.width = value.translation.width + self.pos.width
-                        }
-                        
-                        if abs(yOffsetSpeed(value.translation.height + self.pos.height, 30.0)) <= 30 * scale {
-                            self.position.height = value.translation.height + self.pos.height
-                        }
-                    })
-                    .onEnded({ (value) in
-                        self.pos = self.position
-                    })
-            )
         }
     }
     
-    private let leftEye: (CGFloat, CGFloat) = (61, 133.62)
-    private let rightEye: (CGFloat, CGFloat) = (127, 132.62)
-    private let mouth: (CGFloat, CGFloat) = (35, 19)
-    private let tongue: (CGFloat, CGFloat) = (16, 8)
+    private let keyPoint = [
+        CGPoint(x: -102.00, y: -93.00),
+        CGPoint(x: 0.00, y: 0.00),
+        CGPoint(x: 108, y: 93.00)
+    ]
+    
     private let size: (CGFloat, CGFloat) = (19, 85)
-    private let limit: CGFloat = 30
-    
-    func xOffsetSpeed(_ distance: CGFloat, _ baseDistance: CGFloat) -> CGFloat {
-        let rate: CGFloat = baseDistance / 105.0
-        return rate * distance
-    }
-    
-    func yOffsetSpeed(_ distacne: CGFloat, _ baseDistance: CGFloat) -> CGFloat {
-        let rate: CGFloat = baseDistance / 93.0
-        return rate * distacne
-    }
-    
-    func scaleSpeed(_ distance: CGFloat, _ scale: CGFloat) -> CGFloat {
-        let rate: CGFloat = 0.3 / (105.0 * scale)
-        return rate * distance
-    }
 }
 
 struct BigFacePreView: View {
-    private var pod: CGSize = CGSize()
+    
+    @State private var pod: CGSize = CGSize()
     
     var body: some View {
-        BigFaceView(pos: pod, position: pod)
+        BigFaceView(gesture: $pod)
     }
 }
 
-//struct BigFaceView_Previews: PreviewProvider {
-////    @State private var po: CGSize = .zero
-//    static var previews: some View {
-//        @State private var po: CGSize = .zero
-//        BigFaceView(pos: $po, position: $po)
-////        BigFaceView()
-////            .previewLayout(.fixed(width: 270, height: 270))
-////        BigFaceView()
-////            .previewLayout(.fixed(width: 400, height: 400))
-////        BigFaceView()
-////            .previewLayout(.fixed(width: 400, height: 200))
-//    }
-//}
+struct BigFacePreView_PreView: PreviewProvider {
+    
+    static var previews: some View {
+        BigFacePreView()
+    }
+}
