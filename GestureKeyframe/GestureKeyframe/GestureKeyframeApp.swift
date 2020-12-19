ad9dda3dea93585d9343247f58c2c309177ea7c7
Mayer Lam <mayerdev01@gmail.com>
2020-12-19 16:12:39 +0800

    init commit

---
+++ GestureKeyframe/GestureKeyframe/GestureKeyframeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureKeyframeApp.swift
+//  GestureKeyframe
+//
+//  Created by Mayer Lam on 2020/12/19.
+//
+
+import SwiftUI
+
+@main
+struct GestureKeyframeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
